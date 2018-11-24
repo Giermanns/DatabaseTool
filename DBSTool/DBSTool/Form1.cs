8cf5553cbf4aee8691fc0ae942173528744d935e
CedricGiermanns <32300498+CedricGiermanns@users.noreply.github.com>
2018-11-25 00:10:06 +0100

    First Window Style

--- DBSTool/DBSTool/Form1.cs
+++ DBSTool/DBSTool/Form1.cs
@@ -16,5 +16,20 @@ namespace DBSTool
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox1.Items.AddRange(new object[] {
+                "Insert",
+                "Update",
+                "Select",
+                "Delete"});
+        }
     }
 }
